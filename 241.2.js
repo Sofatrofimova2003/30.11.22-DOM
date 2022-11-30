0c32d3d7d9387c5c9669f565c47d750cc6632a95
Sofatrofimova2003 <114661311+Sofatrofimova2003@users.noreply.github.com>
2022-11-30 17:51:08 +0300

    Add files via upload

---
+++ 241.2.js
@@ -0,0 +1,4 @@
+let block = document.querySelector('#block');
+console.log(block); 
+
+console.log(block); 
---
+++ 241.js
@@ -0,0 +1,15 @@
+
+
+
+let elemen1  = document.querySelector('#elem1');
+let elemen2  = document.querySelector('#elem2');
+let elemen3  = document.querySelector('#elem3');
+console.log(elemen1);
+console.log(elemen2);
+console.log(elemen3);
+
+
+console.log(elemen2.innerText);
+console.log(elemen3.innerText);
+console.log(elemen1.innerText);
+
